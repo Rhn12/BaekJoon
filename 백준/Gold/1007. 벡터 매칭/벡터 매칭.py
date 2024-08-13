a5d294f4b01b1f5de8d04ed7493297d7ba003786
Jun <146902299+Rhn12@users.noreply.github.com>
2024-08-13 10:06:56 +0900

    [Gold II] Title: 벡터 매칭, Time: 920 ms, Memory: 281696 KB -BaekjoonHub

--- 백준/Gold/1007. 벡터 매칭/벡터 매칭.py
+++ 백준/Gold/1007. 벡터 매칭/벡터 매칭.py
@@ -1,75 +1,27 @@
-import os
-import sys
-from itertools import combinations
+import sys, itertools
 
-def main(f=None):
-    init(f)
+input=sys.stdin.readline
+T=int(input())
 
-    def min_vec(l):
-        x_n, y_n = x_t, y_t
-        for x, y in l:
-            x_n -= 2*x
-            y_n -= 2*y
-        return x_n ** 2 + y_n ** 2
-
-    tc = int(input().strip())
-    for _ in range(tc):
-        n = int(input().strip())
-        crd = []
-        x_t, y_t = (-int(i) for i in input().split())
-        for _ in range(n-1):
-            x, y = map(int, input().split())
-            x_t += x
-            y_t += y
-            crd.append((x, y))
-
-        ans = int(8e10)
-
-        for i in combinations(crd, n//2-1):
-            ans = min(ans, min_vec(i))
-        print(pow(ans, 0.5))
-
-enu = enumerate
-
-def For(*args):
-    return itertools.product(*map(range, args))
-def Mat(h, w, default=None):
-    return [[default for _ in range(w)] for _ in range(h)]
-def nDim(*args, default=None):
-    if len(args) == 1:
-        return [default for _ in range(args[0])]
-    else:
-        return [nDim(*args[1:], default=default) for _ in range(args[0])]
-def setStdin(f):
-    global DEBUG, input
-    DEBUG = True
-    sys.stdin = open(f)
-    input = sys.stdin.readline
-def init(f=None):
-    global input
-    input = sys.stdin.readline
-    if os.path.exists("o"):
-        sys.stdout = open("o", "w")
-    if f is not None:
-        setStdin(f)
-    else:
-        if len(sys.argv) == 1:
-            if os.path.isfile("in/i"):
-                setStdin("in/i")
-            elif os.path.isfile("i"):
-                setStdin("i")
-        elif len(sys.argv) == 2:
-            setStdin(sys.argv[1])
-        else:
-            assert False, "Too many sys.argv: %d" % len(sys.argv)
-def pr(*args):
-    if DEBUG:
-        print(*args)
-def pfast(*args, end="\n", sep=' '):
-    sys.stdout.write(sep.join(map(str, args)) + end)
-def parr(arr):
-    for i in arr:
-        print(i)
-
-if __name__ == "__main__":
-    main()
+for _ in range(T):
+    N=int(input())
+    points = []
+    total_x,total_y = 0,0
+ 
+    for _ in range(N):
+        x,y = map(int,input().split())
+        total_x +=x ; total_y += y
+        points.append((x,y))
+ 
+    comb = list(itertools.combinations(points, N//2))
+    ans=3e5
+    
+    for c in comb[:len(comb)//2]:
+        x1,y1 = 0,0
+        for x,y in c:
+            x1 += x ; y1 += y
+        x2,y2 = total_x-x1,total_y-y1
+        
+        hab_vector = ((x2-x1)**2 + (y2-y1)**2)**(0.5)
+        ans=min(ans,hab_vector)
+    print(ans)
