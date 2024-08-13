3589e1d14668701297304c1a8472844672f71fcf
Jun <146902299+Rhn12@users.noreply.github.com>
2024-08-13 10:39:39 +0900

    [Silver II] Title: 제곱수의 합, Time: 4788 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,16 @@
+import sys
+
+def min_square_sum(n):
+    dp = [sys.maxsize] * (n + 1)
+    dp[0] = 0
+    
+    for i in range(1, n + 1):
+        j = 1
+        while j * j <= i:
+            dp[i] = min(dp[i], dp[i - j * j] + 1)
+            j += 1
+    
+    return dp[n]
+
+n = int(input().strip())
+print(min_square_sum(n))
