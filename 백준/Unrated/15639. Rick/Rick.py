78da8212dd6f1f25d0206b605e1ec21d06dd1f2f
Jun <146902299+Rhn12@users.noreply.github.com>
2024-08-13 10:49:55 +0900

    [Unrated] Title: Rick, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Unrated/15639. Rick/Rick.py
@@ -0,0 +1,6 @@
+a = str(input())
+NA = ['Give you up', 'Let you down', 'Run around and desert you', 'Make you cry', 'Say goodbye', 'Tell a lie and hurt you']
+if a in NA:
+    print("NO")
+else:
+    print("YES")
